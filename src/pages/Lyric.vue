d2187b9554717ba422cbe60ab08a76b116f78dc1
何启荣 <22373312>
2024-05-24 23:11:58 +0800

    播放界面初步

--- src/pages/Lyric.vue
+++ src/pages/Lyric.vue
@@ -1,28 +1,41 @@
 <template>
-  <div class="song-lyric">
-    <h1 class="lyric-title">歌词</h1>
-<!--    有歌词-->
-    <ul class="has-lyric" v-if="lyr.length" key="index">
-      <li v-for="(item,index) in lyr" v-bind:key="index">
-        {{item[1]}}
-      </li>
-    </ul>
-<!--    无歌词-->
-    <div v-else class="no-lyric" key="no-lyric">
-      <span>暂无歌词</span>
+  <div class="container">
+    <div class="left-side">
+      <img class="album-image" :src=this.picUrl alt="音乐封面">
+      <div class="music-info">
+        <h2>音乐：{{ this.title }}</h2>
+        <p>歌手：{{ this.artist }}</p>
+      </div>
+    </div>
+
+    <div class="right-side">
+      <h1 class="lyric-title">歌词</h1>
+      <!--    有歌词-->
+      <ul class="has-lyric" v-if="lyr.length" key="index">
+        <li v-for="(item,index) in lyr" v-bind:key="index">
+          {{ item[1] }}
+        </li>
+      </ul>
+      <!--    无歌词-->
+      <div v-else class="no-lyric" key="no-lyric">
+        <span>暂无歌词</span>
+      </div>
     </div>
   </div>
 </template>
 
+
 <script>
 import {mixin} from '../mixins'
 import {mapGetters} from 'vuex'
+
 export default {
   name: 'lyric',
   mixins: [mixin],
-  data () {
+  data() {
     return {
-      lyr: [] // 当前歌曲的歌词
+      lyr: [], // 当前歌曲的歌词
+      lastScrollTime: null, // 记录最近一次手动滚动的时间
     }
   },
   computed: {
@@ -32,9 +45,12 @@ export default {
       'lyric', //  歌词
       'listIndex', // 当前歌曲在歌单中的位置
       'listOfSongs', // 当前歌曲列表
+      'title', // 歌名
+      'artist', // 歌手名
+      'picUrl', // 正在播放的音乐的图片
     ])
   },
-  created () {
+  created() {
     this.lyr = this.lyric
     // console.log(this.lyric)
   },
@@ -56,17 +72,18 @@ export default {
             if (i >= 0) {
               const currentLyric = document.querySelectorAll('.has-lyric li')[i];
               if (currentLyric) {
-                currentLyric.style.color = '#27fdfd';
+                currentLyric.style.color = '#1e66d5';
                 currentLyric.style.fontSize = '25px';
                 currentLyric.style.border = true;
                 currentLyric.classList.add('highlight');
+
               }
             }
           }
         }
       }
     }
-  },
+  }
 }
 </script>
 
