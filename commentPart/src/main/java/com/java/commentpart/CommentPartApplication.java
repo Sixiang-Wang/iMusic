9691e03575434ddd5e1042c422ebf8c4ef6d7267
张温宇翔 <22373297>
2024-08-27 09:14:10 +0800

    更改message和comment的端口号

--- commentPart/src/main/java/com/java/commentpart/CommentPartApplication.java
+++ commentPart/src/main/java/com/java/commentpart/CommentPartApplication.java
@@ -1,11 +1,13 @@
 package com.java.commentpart;
 
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
 @SpringBootApplication
+@MapperScan("com.java.commentpart.dao")
 public class CommentPartApplication {
 
     public static void main(String[] args) {
