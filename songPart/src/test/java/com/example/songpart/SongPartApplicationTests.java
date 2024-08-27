0c88cfe87d8868b80db5dc621d41b0cc047b1781
mcy <machenyu2004@163.com>
2024-08-27 14:57:40 +0800

    修改

---
+++ songPart/src/test/java/com/example/songpart/SongPartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.songpart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SongPartApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
