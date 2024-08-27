a52e54ffab8b2b57d22dc61cbd102edf80ae3003
JiangLeheng <22371210@buaa.edu.cn>
2024-08-27 09:12:55 +0800

    没写完的follow

--- followPart/src/main/java/com/java/followpart/FollowPartApplication.java
+++ followPart/src/main/java/com/java/followpart/FollowPartApplication.java
@@ -1,9 +1,11 @@
 package com.java.followpart;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.java.followpart.dao")
 public class FollowPartApplication {
 
     public static void main(String[] args) {
