4f103b97eec5fedf42aa807d8cacc16d43a1f1d1
mcy <machenyu2004@163.com>
2024-08-28 14:55:10 +0800

    模块

--- songPart/src/main/java/com/java/songPart/service/impl/RecentSongServiceImpl.java
+++ songPart/src/main/java/com/java/songPart/service/impl/RecentSongServiceImpl.java
@@ -12,7 +12,7 @@ import com.java.songPart.utils.Port;
 import com.java.songPart.vo.RecentSongVo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.ParameterizedTypeReference;
-import org.springframework.http.HttpMethod;
+import org.springframework.http.*;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
