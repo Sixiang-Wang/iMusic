549998acfc01aace2464866c9ae265a702184c14
wangsixiang <22373342>
2024-08-29 11:50:11 +0800

    奶奶滴mapper写成Mapper害我好找

--- songPart/src/main/java/com/java/songPart/controller/SongController.java
+++ songPart/src/main/java/com/java/songPart/controller/SongController.java
@@ -19,10 +19,7 @@ import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -36,6 +33,7 @@ import java.util.List;
  */
 @RestController
 @RequestMapping("/song")
+@ResponseBody
 public class SongController {
 
     @Autowired
