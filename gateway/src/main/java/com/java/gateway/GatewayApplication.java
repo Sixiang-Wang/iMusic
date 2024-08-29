3aaab38abe808bc753af09a90ca56b76e156abf0
JiangLeheng <22371210@buaa.edu.cn>
2024-08-29 09:45:36 +0800

    写完的follow

---
+++ gateway/src/main/java/com/java/gateway/GatewayApplication.java
@@ -0,0 +1,15 @@
+package com.java.gateway;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+public class GatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+}
+
